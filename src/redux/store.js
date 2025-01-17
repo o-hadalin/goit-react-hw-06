51043e2e5edf48a3b793f70173b40745fecea10f
O. HADALIN <dyblikat777@gmail.com>
2025-01-17 11:43:05 +0200

    Contact slice + packages

---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contactsSlice';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
+const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
